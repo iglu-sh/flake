a14e5d61ef2f4c4652ff19c0d58381bd01eb6d42
Svenum <s.ziegler@holypenguin.net>
2025-05-20 21:02:53 +0200

    really remove it

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,6 @@
 _: prev: {
   iglu = {
-    iglu-cache = prev.callPackage ./iglu-cache { };
     iglu-controller = prev.callPackage ./iglu-controller { };
-    iglu-cache-docker = prev.callPackage ./iglu-cache-docker { };
     iglu-controller-docker = prev.callPackage ./iglu-controller-docker { };
     flakecheck = prev.callPackage ./flakecheck { };
   };
